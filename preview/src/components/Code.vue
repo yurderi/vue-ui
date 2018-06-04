fd122220d30633351e7595903b63b5f44acb36c8
Tommy Meier <meier@rhinos-media.de>
2018-06-04 11:48:45 +0200

    Improve documentation

--- preview/src/components/Code.vue
+++ preview/src/components/Code.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="code-container">
         <ul class="line-numbers">
-            <li v-for="i in lineNumbers">{{ i }}</li>
+            <li v-for="i in allowedLines">{{ i }}</li>
         </ul>
-        <div class="code" v-highlightjs="formattedCode">
+        <div class="code" v-highlightjs="allowedCode">
             <code :class="language"></code>
         </div>
         <div class="language" v-if="language">
             {{ language }}
         </div>
+        <div class="show-more" v-if="allowedLines < lineNumbers" @click="allowedLines = lineNumbers">
+            Show all
+        </div>
+        <div class="fade-out" v-if="allowedLines < lineNumbers"></div>
     </div>
 </template>
 
@@ -24,15 +28,23 @@ export default {
         },
         lineNumbers() {
             return this.formattedCode.split('\n').length
-        }
+        },
+        allowedCode() {
+            return this.formattedCode
+                .split('\n')
+                .splice(0, this.allowedLines)
+                .join('\n')
+        },
     },
     data: () => ({
-        code: ''
+        code: '',
+        allowedLines: 1
     }),
     mounted() {
         let me = this
     
         me.code = me.$slots.default[ 1 ].text
+        me.allowedLines = me.lineNumbers > 10 ? 10 : me.lineNumbers
     }
 }
 </script>
--- preview/src/views/components/Select.vue
+++ preview/src/views/components/Select.vue
@@ -3,7 +3,8 @@
         <div class="component-example">
             <div class="example-code">
                 <v-code language="html">
-!(<!--<v-select :data="languages" v-model="selectedLanguage" displayField="language">
+!<!--
+<v-select :data="languages" v-model="selectedLanguage" displayField="language" @create="create">
     <template slot="nothing-selected">Please select</template>
     <template slot="selected-value" slot-scope="value">
         {{ value.selectedItem[value.displayField] }}
@@ -17,27 +18,47 @@
         </span>
         {{ i.item.language }}
     </template>
-</v-select>-->
+</v-select>
+-->
                 </v-code>
             </div>
             <div class="example-code">
                 <v-code language="javascript">
 !<!--
-{
-    selectedLanguage: -1,
-    languages: [
-        { id: 1, language: 'Germany', translations: false },
-        { id: 2, language: 'USA', translations: true },
-        { id: 3, language: 'Russia', translations: true },
-        { id: 4, language: 'Italia', translations: true },
-        { id: 5, language: 'Portugal', translations: true },
-        { id: 6, language: 'Polska', translations: true },
-        { id: 7, language: 'Sweden', translations: true },
-        { id: 8, language: 'Switzerland', translations: true },
-        { id: 9, language: 'Luxemburg', translations: true },
-        { id: 10, language: 'Holland', translations: true },
-        { id: 11, language: 'India', translations: true },
-    ]
+export default {
+    comments: true,
+    data: () => ({
+        selectedLanguage: -1,
+        languages: [
+            { id: 1, language: 'Germany', translations: false },
+            { id: 2, language: 'USA', translations: true },
+            { id: 3, language: 'Russia', translations: true },
+            { id: 4, language: 'Italia', translations: true },
+            { id: 5, language: 'Portugal', translations: true },
+            { id: 6, language: 'Polska', translations: true },
+            { id: 7, language: 'Sweden', translations: true },
+            { id: 8, language: 'Switzerland', translations: true },
+            { id: 9, language: 'Luxemburg', translations: true },
+            { id: 10, language: 'Holland', translations: true },
+            { id: 11, language: 'India', translations: true },
+        ]
+    }),
+    methods: {
+        create (value, { spin, done }) {
+            let me = this
+            let item = {
+                id: Math.random(),
+                language: value,
+                translations: false
+            }
+            
+            spin()
+            setTimeout(() => {
+                me.languages.push(item)
+                done(item)
+            }, 500)
+        }
+    }
 }
 -->
                 </v-code>
