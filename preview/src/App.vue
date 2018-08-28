4850214646f973954ac65084ce03628f95aa458c
Tommy Meier <meier@rhinos-media.de>
2018-08-28 13:39:57 +0200

    Add fork-me-on-github image

--- preview/src/App.vue
+++ preview/src/App.vue
@@ -4,6 +4,8 @@
             A simple & functional ui library for vue components.
         </div>
         <router-view />
+        
+        <a href="https://github.com/yurderi/vue-ui"><img style="position: fixed; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
     </div>
 </template>
 
@@ -12,9 +14,3 @@ export default {
     name: 'App'
 }
 </script>
-
-<style lang="less">
-img {
-    margin: 25px 0;
-}
-</style>
